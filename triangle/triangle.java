df932e91279bdebab95e84f3418a132f46ed2bae
Gazolin4ik <zinin.slava.04@mail.ru>
2024-03-18 23:06:29 +0700

    создал класс triangle с методом main для вычисления параметров треугольника

--- triangle/triangle.java
+++ triangle/triangle.java
@@ -1,9 +1,7 @@
 public class triangle {
     // Метод main для демонстрации
     public static void main(String[] args) {
-        double a = 3;
-        double b = 4;
-        double c = 5;
+        double a = 3, b = 4, c = 5;
         double s = (a + b + c) / 2;
         System.out.println("Периметр треугольника: " + (a + b + c));
         System.out.println("Площадь треугольника: " + Math.sqrt(s * (s - a) * (s - b) * (s - c)));
