a46015a2d189a27fce7e882cad54a91dc9a2bed3
Gazolin4ik <zinin.slava.04@mail.ru>
2024-03-22 00:58:55 +0700

    Update rectangle

--- rectangle/rectangle.java
+++ rectangle/rectangle.java
@@ -1,8 +1,33 @@
-public class rectangle { 
-	public static void main(String[] args) { 
-		double a = 10, b = 8;
-		System.out.println("Периметр:" + (2 * a + 2 * b));
-		System.out.println("Площадь:" + (a * b));
-		System.out.println("Диагональ:" + (Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))));
-	}
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    // Функция вычисления периметра
+    public double calculatePerimeter() {
+        return 2 * (width + height);
+    }
+
+    // Функция вычисления площади
+    public double calculateArea() {
+        return width * height;
+    }
+
+    // Функция вычисления длины диагонали
+    public double calculateDiagonal() {
+        return Math.sqrt(width * width + height * height);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("Периметр прямоугольника: " + rectangle.calculatePerimeter());
+        System.out.println("Площадь прямоугольника: " + rectangle.calculateArea());
+        System.out.println("Длина диагонали прямоугольника: " + rectangle.calculateDiagonal());
+    }
 }
