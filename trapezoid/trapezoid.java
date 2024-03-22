2b575b877afb8a73fb9731f29d87faca7de28b79
Gazolin4ik <zinin.slava.04@mail.ru>
2024-03-22 09:57:17 +0700

    Update trapezoid

--- trapezoid/trapezoid.java
+++ trapezoid/trapezoid.java
@@ -1,10 +1,33 @@
 public class trapezoid {
-	public static void main(String[] args) {
-		double a = 10, b = 6, c = 5, d = 5, h = 4;
-		System.out.println("Perimeter: " + (a + b + c + d));
-		System.out.println("Area: " + ((a + b) /  2 * h));
-		System.out.println("MedianLength: " + ((a + b) / 2));
-	}
-}
+    double side1;
+    double side2;
+    double side3;
+    double side4;
+    double height;
 
+
+    public trapezoid(double a, double b, double c, double d, double h) { // конструктор, проверка на корректность даных
+        if (a <= 0 || b <=0 || c <= 0 || d <= 0 || h <= 0) {
+            throw new IllegalArgumentException("Стороны трапеции и её высота должны быть положительными!");
+        }
+        this.side1 = a;
+        this.side2 = b;
+	this.side3 = c;
+	this.side4 = d;
+	this.height = h;
+    }
+
+    public double getPerimeter() {
+        return side1 + side2 + side3 + side4;
+    } // периметр
+
+    public double getArea() {
+        return ((side1 + side2) / 2) * height;;
+    } // площадь трапеции
+
+    public double getLenMidLine() { 
+	// Длина средней линии
+        return (side1 + side2) / 2;
+    } 
+}
 
